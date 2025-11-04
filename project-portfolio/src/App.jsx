ef03fc28f98cf2fbb8a3a3845ab5b39fecaaa9dd
deniaverkade <9024406@student.zadkine.nl>
2025-11-04 13:16:13 +0100

    US1: React Router setup en navigatielinks

--- project-portfolio/src/App.jsx
+++ project-portfolio/src/App.jsx
@@ -1,14 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import NoPage from "./Pages/NoPage";
+import Project from "./Pages/Project";
+import ProjectDetail from "./Pages/ProjectDetail";
+import Contact from "./Pages/Contact";
 import './App.css'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
