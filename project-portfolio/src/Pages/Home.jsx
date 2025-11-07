32ff7987fd972fc371405b3ace205c6e32a56914
deniaverkade <9024406@student.zadkine.nl>
2025-11-07 13:20:55 +0100

    a

--- project-portfolio/src/Pages/Home.jsx
+++ project-portfolio/src/Pages/Home.jsx
@@ -3,7 +3,16 @@
 const Home = () => {
     return ( 
     <div className="homepage">
-<h1>home</h1>
+        <div className="imgcontainer">
+            <img className="h_img1" src="../public/images/homepage_img1.jpg"/>
+            <img className="h_img2" src="../public/images/homepage_img2.jpg"/>
+            <img className="h_img3" src="../public/images/homepage_img3.jpg"/>
+        </div>
+        <img className="homepagepic" src="../public/images/homepage_profile.jpg"/>
+        <div className="homepagebox">
+        <h1 className="homepageh1">Portfolio</h1>
+        <h2 className="homepageh2">Denia Verkade</h2>
+        </div>
     </div>
      );
 }
