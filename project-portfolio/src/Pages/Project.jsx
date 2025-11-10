4f7a8ba41581c45d59d6afa28e2e2c229b4b3083
deniaverkade <9024406@student.zadkine.nl>
2025-11-10 11:54:50 +0100

    a

--- project-portfolio/src/Pages/Project.jsx
+++ project-portfolio/src/Pages/Project.jsx
@@ -1,19 +1,23 @@
 import { Link, Outlet } from 'react-router-dom';
-
+import {projects} from "../projects";
 
 
 const Project = () => {
     return ( <>
+    
     <div className="projectbody">
-        <h1>PROJECTEN</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed risus tellus, auctor venenatis elementum ornare, consectetur vel mi. Maecenas gravida, augue non gravida porta, lectus felis tincidunt lectus, sed vehicula justo mi nec erat. Donec vel tempor massa, in posuere turpis. Morbi hendrerit aliquet malesuada. Sed placerat ornare dolor at dapibus. Integer mauris leo, faucibus in turpis at, pharetra malesuada libero. Sed maximus iaculis tortor placerat sollicitudin</p>
+        <div className="sideboxproject">
+            <h1>PROJECTEN</h1>
+        <p className="projectp">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed risus tellus, auctor venenatis elementum ornare, consectetur vel mi. Maecenas gravida, augue non gravida porta, lectus felis tincidunt lectus, sed vehicula justo mi nec erat. Donec vel tempor massa, in posuere turpis. Morbi hendrerit aliquet malesuada. Sed placerat ornare dolor at dapibus. Integer mauris leo, faucibus in turpis at, pharetra malesuada libero. Sed maximus iaculis tortor placerat sollicitudin</p>
         <h2>FILTERS</h2>
         <button>RESET FILTER</button>
+        </div>
+        
     </div>
         
         <div className="projectlist">
 <h1>Project pagina</h1>
-        <p><Link to="/projectdetail">Testlink voor detailpagina</Link></p>
+        <p className="projectp"><Link to="/projectdetail">Testlink voor detailpagina</Link></p>
         </div>
     
     </> );
--- project-portfolio/src/projects.js
+++ project-portfolio/src/projects.js
@@ -1,14 +1,10 @@
 const projects = [
-    {id: 1, name:"placeholder1", link:"placeholderurl1", imgurl:"placeholderimg1"},
-    {id: 2, name:"placeholder2", link:"placeholderurl2", imgurl:"placeholderimg2"},
-    {id: 3, name:"placeholder3", link:"placeholderurl3", imgurl:"placeholderimg3"},
-    {id: 4, name:"placeholder4", link:"placeholderurl4", imgurl:"placeholderimg4"},
-    {id: 5, name:"placeholder5", link:"placeholderurl5", imgurl:"placeholderimg5"},
-    {id: 6, name:"placeholder6", link:"placeholderurl6", imgurl:"placeholderimg6"},
+    {id: 1, name:"placeholder1", link:"placeholderurl1", imgurl:"placeholderimg1",tags:["tag1","tag2","tag3"]},
+    {id: 2, name:"placeholder2", link:"placeholderurl2", imgurl:"placeholderimg2",tags:["tag1","tag2","tag3"]},
+    {id: 3, name:"placeholder3", link:"placeholderurl3", imgurl:"placeholderimg3",tags:["tag1","tag2","tag3"]},
+    {id: 4, name:"placeholder4", link:"placeholderurl4", imgurl:"placeholderimg4",tags:["tag1","tag2","tag3"]},
+    {id: 5, name:"placeholder5", link:"placeholderurl5", imgurl:"placeholderimg5",tags:["tag1","tag2","tag3"]},
+    {id: 6, name:"placeholder6", link:"placeholderurl6", imgurl:"placeholderimg6",tags:["tag1","tag2","tag3"]},
 ];
 
-const tags = [
-    {id: 1, name:"tag1"},
-    {id: 2, name:"tag2"},
-    {id: 3, name:"tag3"},
-]
+export default projects;
